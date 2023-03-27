cc7c14371563e0007d7884dcf397204ba767179c
Sergei Demin <wsch@bk.ru>
2023-03-28 00:27:28 +0200

    firserror

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -14,4 +14,14 @@ public class CashbackHackServiceTest {
     }
 
 
+        @org.testng.annotations.Test
+        public void testRemain() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int actual = service.remain(amount);
+            int expected = 0;
+            assertEquals(actual, expected);
+        }
+
+
 }
