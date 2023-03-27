96daba1b420aa90463f26b6d862c78d8f1ecd6da
Sergei Demin <wsch@bk.ru>
2023-03-27 23:49:12 +0200

    firstng

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void test2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+
+}
