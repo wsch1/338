51041936877f8cdab6636704df02d53a0718b445
Sergei Demin <wsch@bk.ru>
2023-03-28 00:29:34 +0200

    firserror

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -13,5 +13,14 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
+    @org.junit.Test
+    public void test1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
 
 }
